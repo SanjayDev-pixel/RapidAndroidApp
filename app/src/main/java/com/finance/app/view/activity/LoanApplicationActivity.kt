a580f57085e61891b951b2e0830515f7425a84b3
VISHR93 <rathivishal.rathi223@gmail.com>
2019-09-02 15:58:01 +0530

    personal information screen created

--- app/src/main/java/com/finance/app/view/activity/LoanApplicationActivity.kt
+++ app/src/main/java/com/finance/app/view/activity/LoanApplicationActivity.kt
@@ -86,11 +86,4 @@ class LoanApplicationActivity : BaseAppCompatActivity() {
             isExpand = true
         }
     }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-//        for (fragment in supportFragmentManager.fragments) {
-//            fragment.onActivityResult(requestCode, resultCode, data)
-//        }
-    }
-}
+ }
--- app/src/main/java/com/finance/app/view/fragment/IncomeFragment.kt
+++ app/src/main/java/com/finance/app/view/fragment/IncomeFragment.kt
@@ -23,51 +23,7 @@ class IncomeFragment : androidx.fragment.app.Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        showTableLayout()
+//        showTableLayout()
     }
 
-    private fun showTableLayout() {
-        val tbrow0 = TableRow(context)
-        val tv0 = TextView(context)
-        tv0.text = " Sl.No "
-        tv0.setTextColor(Color.BLACK)
-        tbrow0.addView(tv0)
-        val tv1 = TextView(context)
-        tv1.text = " Product "
-        tv1.setTextColor(Color.BLACK)
-        tbrow0.addView(tv1)
-        val tv2 = TextView(context)
-        tv2.text = " Unit Price "
-        tv2.setTextColor(Color.BLACK)
-        tbrow0.addView(tv2)
-        val tv3 = TextView(context)
-        tv3.text = " Stock Remaining "
-        tv3.setTextColor(Color.BLACK)
-        tbrow0.addView(tv3)
-        binding.tableFinancialYear.addView(tbrow0)
-        for (i in 0..24) {
-            val tbrow = TableRow(context)
-            val t1v = TextView(context)
-            t1v.text = "" + i
-            t1v.setTextColor(Color.BLACK)
-            t1v.gravity = Gravity.CENTER
-            tbrow.addView(t1v)
-            val t2v = TextView(context)
-            t2v.text = "Product $i"
-            t2v.setTextColor(Color.BLACK)
-            t2v.gravity = Gravity.CENTER
-            tbrow.addView(t2v)
-            val t3v = TextView(context)
-            t3v.text = "Rs.$i"
-            t3v.setTextColor(Color.BLACK)
-            t3v.gravity = Gravity.CENTER
-            tbrow.addView(t3v)
-            val t4v = TextView(context)
-            t4v.text = "" + i * 15 / 32 * 10
-            t4v.setTextColor(Color.BLACK)
-            t4v.gravity = Gravity.CENTER
-            tbrow.addView(t4v)
-            binding.tableFinancialYear.addView(tbrow)
-        }
-    }
 }
