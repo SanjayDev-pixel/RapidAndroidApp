fa2d2864e62a4e5a98b9589b596e2edc8ffd9297
008AjayRawat <ajayrawat.android@gmail.com>
2020-02-04 17:13:43 +0530

    LeadDataView Model Bug Fix

--- app/src/main/java/com/finance/app/utility/LeadRequestResponseConversion.kt
+++ app/src/main/java/com/finance/app/utility/LeadRequestResponseConversion.kt
@@ -11,17 +11,21 @@ class LeadRequestResponseConversion {
 
     private val gson = Gson()
 
-    fun getResponseObject(form: AppEnums.FormType, response: Response.LoanApplicationGetObj): Serializable? {
+    fun getResponseObject(form: AppEnums.FormType, response: Response.LoanApplicationGetObj?): Serializable? {
 
-        return when (form) {
-                LOANINFO -> gson.fromJson(response.draftData, LoanInfoModel::class.java)
-                PERSONALINFO -> gson.fromJson(response.draftData, PersonalApplicantList::class.java)
-                EMPLOYMENT -> gson.fromJson(response.draftData, EmploymentApplicantList::class.java)
-                BANKDETAIL -> gson.fromJson(response.draftData, BankDetailList::class.java)
-                LIABILITYASSET -> gson.fromJson(response.draftData, AssetLiabilityList::class.java)
-                PROPERTY -> gson.fromJson(response.draftData, PropertyModel::class.java)
-                REFERENCE -> gson.fromJson(response.draftData, ReferencesList::class.java)
+            response?.let {
+                return when (form) {
+                    LOANINFO -> gson.fromJson(response.draftData, LoanInfoModel::class.java)
+                    PERSONALINFO -> gson.fromJson(response.draftData, PersonalApplicantList::class.java)
+                    EMPLOYMENT -> gson.fromJson(response.draftData, EmploymentApplicantList::class.java)
+                    BANKDETAIL -> gson.fromJson(response.draftData, BankDetailList::class.java)
+                    LIABILITYASSET -> gson.fromJson(response.draftData, AssetLiabilityList::class.java)
+                    PROPERTY -> gson.fromJson(response.draftData, PropertyModel::class.java)
+                    REFERENCE -> gson.fromJson(response.draftData, ReferencesList::class.java)
+                }
             }
+
+            return null
     }
 
 
