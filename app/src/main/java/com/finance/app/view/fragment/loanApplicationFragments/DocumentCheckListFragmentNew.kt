a00b85db7dc886645acc5d5d9e0ba606d6c6841c
sandeep <sandeep@idreambiz.com>
2020-02-10 11:14:45 +0530

    documentchecklistFragmentNew working

--- app/src/main/java/com/finance/app/view/fragment/loanApplicationFragments/DocumentCheckListFragmentNew.kt
+++ app/src/main/java/com/finance/app/view/fragment/loanApplicationFragments/DocumentCheckListFragmentNew.kt
@@ -10,15 +10,19 @@ import com.finance.app.databinding.FragmentDocumentChecklistBinding
 import com.finance.app.eventBusModel.AppEvents
 import com.finance.app.persistence.model.PersonalApplicantsModel
 import com.finance.app.presenter.presenter.Presenter
+import com.finance.app.presenter.presenter.ViewGeneric
 import com.finance.app.utility.LeadMetaData
-import com.finance.app.view.adapters.recycler.adapter.AssetLiabilityPagerAdapter
+import com.finance.app.view.activity.SyncActivity
 import com.finance.app.view.adapters.recycler.adapter.DocumentCheckLIstPagerAdapter
 import motobeans.architecture.application.ArchitectureApp
+import motobeans.architecture.constants.Constants
 import motobeans.architecture.constants.ConstantsApi
 import motobeans.architecture.customAppComponents.activity.BaseFragment
 import motobeans.architecture.development.interfaces.DataBaseUtil
 import motobeans.architecture.development.interfaces.FormValidation
 import motobeans.architecture.development.interfaces.SharedPreferencesUtil
+import motobeans.architecture.retrofit.request.Requests
+import motobeans.architecture.retrofit.response.Response
 import java.util.ArrayList
 import javax.inject.Inject
 
@@ -106,4 +110,36 @@ class DocumentCheckListFragmentNew : BaseFragment(){
         binding.tabLead.setupWithViewPager(binding.viewPager)
     }
 
+/*
+    inner class CallFinalSubmit: ViewGeneric<String, Response.ResponseGetLoanApplication>(context = context) {
+        override val apiRequest: Requests.RequestFinalSubmit
+            get() = LeadMetaData.getLeadId().toString()
+
+        private val mRequest: Requests.RequestFinalSubmit
+            get() {
+
+
+                return Requests.RequestFinalSubmit()
+            }
+
+
+
+        override fun getApiSuccess(value: Response.ResponseGetLoanApplication) {
+            if (value.responseCode == Constants.SUCCESS) {
+                *//*sharedPreferences.saveLoginData(value)
+                SyncActivity.start(this@LoginActivity)*//*
+            } else {
+                showToast(value.responseMsg)
+            }
+        }
+
+        override fun getApiFailure(msg: String) {
+            showToast(msg)
+        }
+    }*/
+
+
+
 }
+
+
