b0025d1c81f86cf25481c47a2104904a14a15063
sandeep <sandeep@idreambiz.com>
2020-03-05 18:22:03 +0530

    preview screen city name state name bug fix

--- app/src/main/java/com/finance/app/view/adapters/recycler/holder/PreviewDocumentsHolder.kt
+++ app/src/main/java/com/finance/app/view/adapters/recycler/holder/PreviewDocumentsHolder.kt
@@ -1,11 +1,10 @@
 package com.finance.app.view.adapters.recycler.holder
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.view.View
-import android.widget.ProgressBar
 import android.widget.Toast
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.finance.app.databinding.PreviewLayoutDocumentChecklistBinding
 import com.finance.app.persistence.model.AllLeadMaster
@@ -14,37 +13,50 @@ import com.finance.app.persistence.model.PersonalApplicantsModel
 import com.finance.app.presenter.presenter.Presenter
 import com.finance.app.presenter.presenter.ViewGeneric
 import com.finance.app.utility.LeadMetaData
-import com.finance.app.view.activity.FinalSubmitActivity
 import com.finance.app.view.activity.LoanSubmitStatusActivity
 import com.finance.app.viewModel.AppDataViewModel
-import com.finance.app.viewModel.LeadDataViewModel
-import motobeans.architecture.appDelegates.ViewModelType
+import kotlinx.android.synthetic.main.preview_layout_document_checklist.view.*
 import motobeans.architecture.constants.Constants
 import motobeans.architecture.constants.ConstantsApi
 import motobeans.architecture.retrofit.request.Requests
 import motobeans.architecture.retrofit.response.Response
 import motobeans.architecture.util.exIsNotEmptyOrNullOrBlank
 
+
 class PreviewDocumentsHolder(val binding: PreviewLayoutDocumentChecklistBinding, val mContext: Context)
     : RecyclerView.ViewHolder(binding.root) {
 
     private val presenter = Presenter()
+    var lastIndex:Int=0
+    var listSize:Int=0
     // private var progressBar: ProgressBar? = null// temp
 
     fun bindItems(list: ArrayList<PersonalApplicantsModel>?, pos: Int, viewModel: AppDataViewModel) {
 
         if (!list.isNullOrEmpty()) {
-            setValueInCard(list[pos])
+
+             listSize=list.size
+            lastIndex= list.lastIndex
+            setValueInCard(list[pos],listSize,lastIndex)
+
         }
     }
 
-    private fun setValueInCard(data: PersonalApplicantsModel) {
+    private fun setValueInCard(data: PersonalApplicantsModel,listSize:Int,lastIndex:Int) {
 
-        if (data.isMainApplicant == true) {
-            binding.btnSumbitlead.visibility = View.VISIBLE
+        if (data.isMainApplicant == true && listSize==1) {
+
+            binding.layoutSubmitlead.visibility=View.VISIBLE
+        }else if(data.isMainApplicant == false && listSize>1){
+
+            binding.layoutSubmitlead.visibility=View.VISIBLE
         }
 
-        binding.btnSumbitlead.setOnClickListener() {
+
+
+
+
+        binding.layoutSubmitlead.btnSubmit.setOnClickListener() {
 
             if (LeadMetaData.getLeadData()?.status == "Submitted") {
                 Toast.makeText(mContext, "Lead is already submitted.", Toast.LENGTH_SHORT).show()
@@ -55,6 +67,11 @@ class PreviewDocumentsHolder(val binding: PreviewLayoutDocumentChecklistBinding,
 
             }
 
+        }
+
+        binding.layoutSubmitlead.btnCancel.setOnClickListener(){
+
+            (mContext as Activity).finish()
         }
 
     }
@@ -78,7 +95,7 @@ class PreviewDocumentsHolder(val binding: PreviewLayoutDocumentChecklistBinding,
             if (value.responseCode == Constants.SUCCESS) {
                 Toast.makeText(context, "Submitted Successfully.", Toast.LENGTH_SHORT).show()
 
-                //   progressBar!!.visibility = View.GONE
+                   binding.progressBar!!.visibility = View.GONE
 
                 val submitLoanResponse: ApplicantionSubmitModel? = value.responseObj
 
@@ -96,7 +113,7 @@ class PreviewDocumentsHolder(val binding: PreviewLayoutDocumentChecklistBinding,
 
             } else {
                 showToast(value.responseMsg)
-                //  progressBar!!.visibility = View.GONE
+                  binding.progressBar!!.visibility = View.GONE
             }
 
         }
@@ -105,13 +122,13 @@ class PreviewDocumentsHolder(val binding: PreviewLayoutDocumentChecklistBinding,
 
             if (msg.exIsNotEmptyOrNullOrBlank()) {
                 super.getApiFailure(msg)
-                //   progressBar!!.visibility = View.GONE
+                   binding.progressBar!!.visibility = View.GONE
             } else {
                 super.getApiFailure("Time out Error")
-                // progressBar!!.visibility = View.GONE}
+                binding.progressBar!!.visibility = View.GONE}
 
             }
-        }
+
 
         private fun getCallUpdateRequest(): Requests.RequestFinalSubmit? {
             val leadId = LeadMetaData.getLeadId()
@@ -121,3 +138,4 @@ class PreviewDocumentsHolder(val binding: PreviewLayoutDocumentChecklistBinding,
 
     }
 }
+
